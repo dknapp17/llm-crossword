0f6b4fa971f31cc35dc0e9ee011ef9594ce3a6d6
David Knapp <dsknapp17@gmail.com>
2026-05-29 14:51:22 -0400

    checkpoint

--- scripts/ingest_crossword.py
+++ scripts/ingest_crossword.py
@@ -10,7 +10,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from src.pipeline.ingestion import CrosswordGridParser
+from src.pipeline.ingestion import CrosswordClueAnswerParser, CrosswordGridParser
 
 url = 'https://www.xwordinfo.com/Crossword?date=4/19/2026'
 headers = {
@@ -27,6 +27,8 @@ soup = BeautifulSoup(response.text, features='html.parser')
 
 puz_html = soup.find("table", id="PuzTable")
 
+clue_ans_html = soup.find("div", id="CPHContent_ClueBox")
+
 # step1: parse clue, answer key value pairs
 # step2: parse grid to get locations of all squares
 # step3: use clue/answer and grid objects to get CrosswordClue and CrosswordAnswer pairs
@@ -36,7 +38,18 @@ puz_html = soup.find("table", id="PuzTable")
     # and all squares below until black or border
 
 # parse puzzle
-parser = CrosswordGridParser()
-grid = parser.parse(puz_html)
+clue_answer_parser = CrosswordClueAnswerParser()
+clue_answer_pairs = clue_answer_parser.parse(clue_ans_html)
 
-print(grid.squares[47])
+grid_parser = CrosswordGridParser()
+grid = grid_parser.parse(puz_html)
+
+# print(grid.squares[47])
+# print(clue_ans_html)
+print(clue_answer_pairs[0].crossword_clue.text)
+print(clue_answer_pairs[0].crossword_answer.text)
+
+# now we have a grid (collection of squares and a collection of clue answer pairs)
+# use these together to get SolverClueInput and SolverAnswer
+
+
--- src/pipeline/ingestion.py
+++ src/pipeline/ingestion.py
@@ -1,9 +1,108 @@
 from bs4.element import Tag
 
 from src.domain.crossword import (
+    AcrossDown,
+    CrosswordAnswer,
+    CrosswordClue,
+    CrosswordClueAnswerPair,
     CrosswordGrid,
     CrosswordGridSquare,
 )
+
+
+class CrosswordClueAnswerParser:
+
+    def parse(
+        self,
+        html_cluebox: Tag,
+    ) -> list[CrosswordClueAnswerPair]:
+
+        across_pairs = self._parse_direction(
+            html_cluebox.find("div", id="ACluesPan"),
+            AcrossDown.ACROSS,
+        )
+
+        down_pairs = self._parse_direction(
+            html_cluebox.find("div", id="DCluesPan"),
+            AcrossDown.DOWN,
+        )
+
+        return across_pairs + down_pairs
+
+    # -------------------------
+    # Direction level
+    # -------------------------
+    def _parse_direction(
+        self,
+        direction_div: Tag,
+        across_down: AcrossDown,
+    ) -> list[CrosswordClueAnswerPair]:
+
+        if direction_div is None:
+            return []
+
+        numclue_div = direction_div.find("div", class_="numclue")
+
+        if numclue_div is None:
+            return []
+
+        children = numclue_div.find_all("div", recursive=False)
+
+        clue_answer_pairs = []
+
+        for idx in range(0, len(children), 2):
+
+            clue_num = int(children[idx].text.strip())
+
+            clue_container = children[idx + 1]
+
+            clue_text = self._extract_clue_text(clue_container)
+            answer_text = self._extract_answer_text(clue_container)
+
+            crossword_clue = CrosswordClue(
+                text=clue_text,
+                across_down=across_down,
+                clue_num=clue_num,
+            )
+
+            crossword_answer = CrosswordAnswer(
+                text=answer_text,
+                across_down=across_down,
+                clue_num=clue_num,
+            )
+
+            clue_answer_pairs.append(
+                CrosswordClueAnswerPair(
+                    crossword_clue=crossword_clue,
+                    crossword_answer=crossword_answer,
+                )
+            )
+
+        return clue_answer_pairs
+
+    # -------------------------
+    # Atom logic
+    # -------------------------
+    def _extract_clue_text(
+        self,
+        clue_container: Tag,
+    ) -> str:
+
+        clue_text = clue_container.contents[0]
+
+        return clue_text.replace(" : ", "").strip()
+
+    def _extract_answer_text(
+        self,
+        clue_container: Tag,
+    ) -> str:
+
+        answer_link = clue_container.find("a")
+
+        if answer_link is None:
+            raise ValueError("Could not find answer link")
+
+        return answer_link.text.strip()
 
 
 class CrosswordGridParser:
