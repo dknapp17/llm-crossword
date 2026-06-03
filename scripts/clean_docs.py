75b43e5f3c0d8f6c30e9375c7a7ca33afb59edcd
David Knapp <dsknapp17@gmail.com>
2026-06-03 18:56:16 -0400

    clean up

--- scripts/clean_docs.py
+++ scripts/clean_docs.py
@@ -1,9 +1,3 @@
-#TODO: modify cleaning.py to take all docs from mongo and clean. 
-# worry about storage in quadrant later
-
-# query data warehouse
-# clean documents
-# load to vector db
 from llm_cw.domain.documents import CleanCrosswordDocument, CrosswordDocument
 from llm_cw.preprocessing.cleaning import clean_documents
 
